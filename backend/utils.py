8e8e96d04e51b085e5d7fbcb3fb865caf7ddfaf6
Jason Kuan (Astra) <jason9075@gmail.com>
2020-11-11 14:21:31 +0800

    hotfix freeze only load pytorch weight

--- backend/utils.py
+++ backend/utils.py
@@ -10,7 +10,6 @@ def load_weight(model, torch_path, trainable=True, verbose=False):
     #     mapping_table = {layer['name']: layer['weight'] for layer in mapping_table}
 
     for layer in model.layers:
-        layer.trainable = trainable
         if verbose:
             print(f'Set layer: {layer.name}')
         layer_prefix = layer.name.split('__')[0]
@@ -20,21 +19,25 @@ def load_weight(model, torch_path, trainable=True, verbose=False):
             weight = np.array(torch_weights[f'{layer_prefix}.weight'])
             weight = np.transpose(weight, [2, 3, 1, 0])
             layer.set_weights([weight])
+            layer.trainable = trainable
         elif layer_type == 'bn':
             gamma = np.array(torch_weights[f'{layer_prefix}.weight'])
             beta = np.array(torch_weights[f'{layer_prefix}.bias'])
             running_mean = np.array(torch_weights[f'{layer_prefix}.running_mean'])
             running_var = np.array(torch_weights[f'{layer_prefix}.running_var'])
             layer.set_weights([gamma, beta, running_mean, running_var])
+            layer.trainable = trainable
         elif layer_type == 'convbias':
             weight = np.array(torch_weights[f'{layer_prefix}.weight'])
             bias = np.array(torch_weights[f'{layer_prefix}.bias'])
             weight = np.transpose(weight, [2, 3, 1, 0])
             layer.set_weights([weight, bias])
+            layer.trainable = trainable
         elif layer_type == 'fc':
             weight = np.array(torch_weights[f'{layer_prefix}.weight'])
             bias = np.array(torch_weights[f'{layer_prefix}.bias'])
             layer.set_weights([weight, bias])
+            layer.trainable = trainable
         else:
             if verbose:
                 print(f'Ignore layer: {layer.name}')
