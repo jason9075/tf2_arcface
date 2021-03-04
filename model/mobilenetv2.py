26aea5a6660ef83e1e87543a0733ee75d6923327
Jason Kuan (Astra) <jason9075@gmail.com>
2021-03-04 11:57:53 +0800

    hotfix
    raise ValueError('The `weights` argument should be either '
    ValueError: The `weights` argument should be either `None` (random initialization), `imagenet` (pre-training on ImageNet), or the path to the weights file to be loaded.

--- model/mobilenetv2.py
+++ model/mobilenetv2.py
@@ -2,6 +2,6 @@ import tensorflow as tf
 
 
 def create_mobilenetv2(input_node):
-    model = tf.keras.applications.MobileNetV2(input_tensor=input_node, include_top=False, weights='None')
+    model = tf.keras.applications.MobileNetV2(input_tensor=input_node, include_top=False, weights=None)
 
     return model.output
